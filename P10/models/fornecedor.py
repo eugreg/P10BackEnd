06d3d6ee692ab0c4d584fa1c40361fa23a39049e
MarceloHarbs <mharbs614@gmail.com>
2023-08-15 13:53:11 -0300

    Data Base - Fornecedores

--- P10/models/fornecedor.py
+++ P10/models/fornecedor.py
@@ -7,6 +7,7 @@ class Fornecedor(models.Model):
     email = models.EmailField(null=False, blank=False, default=None)
     telefone = models.CharField(max_length=100)
     endereco = models.CharField(max_length=100)
+    cep = models.CharField(max_length=100   )
 
     def __str__(self):
         return self.nome
--- P10/models/sazonal.py
+++ P10/models/sazonal.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Sazonal(models.Model):
-    descrição = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descrição
+        return self.descricao
         
     class Meta:
         verbose_name_plural = "Sazonais"
