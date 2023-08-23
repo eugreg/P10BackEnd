ce28197da7c0d67a8de90570917cb609f9270ef4
MarceloHarbs <mharbs614@gmail.com>
2023-08-23 16:05:28 -0300

    Model SubCategoria

--- P10/models/produto.py
+++ P10/models/produto.py
@@ -8,14 +8,14 @@ from .sazonal import Sazonal
 
 
 class Produtos(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=150)
+    nome = models.CharField(max_length=100, null=False)
+    descricao = models.CharField(max_length=150, null=False)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     quantidade = models.IntegerField(null=True, default=0)
-    fornecedor = models.ForeignKey(Fornecedor,on_delete=models.PROTECT,related_name="Produtos")
-    categoria = models.ForeignKey(Categoria,on_delete=models.PROTECT,related_name="Produtos")
+    fornecedor = models.ForeignKey(Fornecedor,on_delete=models.PROTECT, null=False, related_name="Produtos")
+    categoria = models.ForeignKey(Categoria,on_delete=models.PROTECT, null=False, related_name="Produtos")
     sazonal = models.ForeignKey(Sazonal, on_delete=models.PROTECT, null=True, related_name="Produtos")
-    desconto = models.ForeignKey(Descontos,on_delete=models.PROTECT,related_name="Produtos")
+    desconto = models.ForeignKey(Descontos,on_delete=models.PROTECT, null=True, related_name="Produtos")
     capa = models.ForeignKey(
         Image,
         related_name="+",
--- P10/models/subcategoria.py
+++ P10/models/subcategoria.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from .categoria import Categoria
+
 class SubCategoria(models.Model):
     descricao = models.CharField(max_length=100)
-
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, null=True, related_name="subcategorias")
+    
     def __str__(self):
         return self.descricao
