bf3c809841031dc7d78bad490764edb4547dd7c9
eugreg <daiaegregory@gmail.com>
2023-08-29 09:20:11 -0300

    criacao de intens compra

--- P10/serializers/__init__.py
+++ P10/serializers/__init__.py
@@ -4,4 +4,4 @@ from .categoria import  CategoriaSerializer
 from .descontos import DescontoSerializer
 from .subcategoria import SubCategoriaSerializer
 from .sazonal import SazonalSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer,ItensCompraSerializer
--- P10/serializers/compra.py
+++ P10/serializers/compra.py
@@ -1,10 +1,17 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from P10.models import Compra
+from P10.models import Compra, ItensCompra
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = "__all__"
+        
 
 class CompraSerializer(ModelSerializer):
     class Meta:
         status = CharField(source="get_status_display", read_only=True)
         usuario = CharField(source="usuario.username", read_only=True)
+        itens = ItensCompraSerializer(many=True, read_only=True)
         model = Compra
         fields = "__all__"
