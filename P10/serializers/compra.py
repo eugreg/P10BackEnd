1a85e3b9613873721a80376c812404b0f0396d4a
eugreg <daiaegregory@gmail.com>
2023-08-29 14:18:50 -0300

    total

--- P10/serializers/compra.py
+++ P10/serializers/compra.py
@@ -1,18 +1,23 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
 from P10.models import Compra, ItensCompra
 
 class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
     class Meta:
         model = ItensCompra
-        fields = ["produto", "quantidade"]
-        depth = 2
+        fields = ["produto", "quantidade", "total" ] 
+        depth = 1
+    
+    def get_total(self, obj):
+        return obj.produto.preco * obj.quantidade
         
 
 class CompraSerializer(ModelSerializer):
+    status = CharField(source="get_status_display", read_only=True)
+    usuario = CharField(source="usuario.username", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
+    
     class Meta:
-        status = CharField(source="get_status_display", read_only=True)
-        usuario = CharField(source="usuario.username", read_only=True)
-        itens = ItensCompraSerializer(many=True, read_only=True)
         model = Compra
         fields = "__all__"
