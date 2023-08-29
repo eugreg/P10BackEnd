c31a4987a26c8281377e58a1c0cf940be5afe3f3
eugreg <daiaegregory@gmail.com>
2023-08-29 09:04:28 -0300

    criacao  de status no serializer

--- P10/serializers/compra.py
+++ P10/serializers/compra.py
@@ -4,6 +4,7 @@ from P10.models import Compra
 
 class CompraSerializer(ModelSerializer):
     class Meta:
+        status = CharField(source="get_status_display", read_only=True)
         usuario = CharField(source="usuario.username", read_only=True)
         model = Compra
         fields = "__all__"
