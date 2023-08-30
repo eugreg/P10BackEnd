4d36dfb755f4fd5a7ca15a38cc27cd19dddfae40
eugreg <daiaegregory@gmail.com>
2023-08-30 15:56:36 -0300

    alteracao em compras

--- P10/serializers/compra.py
+++ P10/serializers/compra.py
@@ -1,24 +1,30 @@
-from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
+from rest_framework.serializers import (
+    ModelSerializer,
+    CharField,
+    SerializerMethodField,
+)
 from rest_framework import serializers
 
 from P10.models import Compra, ItensCompra
 
+
 class ItensCompraSerializer(ModelSerializer):
     total = SerializerMethodField()
+
     class Meta:
         model = ItensCompra
-        fields = ["produto", "quantidade", "total" ] 
+        fields = ["produto", "quantidade", "total"]
         depth = 2
-    
+
     def get_total(self, instance):
         return instance.quantidade * instance.preco_item
-        
+
 
 class CompraSerializer(ModelSerializer):
     status = CharField(source="get_status_display", read_only=True)
     usuario = CharField(source="usuario.email", read_only=True)
     itens = ItensCompraSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Compra
         fields = ("id", "usuario", "status", "total", "itens")
@@ -27,12 +33,14 @@ class CompraSerializer(ModelSerializer):
 class CriarEditarItenSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
-        fields = ["produto", "quantidade" ]
+        fields = ["produto", "quantidade"]
 
     def validate(self, data):
         if data["quantidade"] > data["produto"].quantidade:
             raise serializers.ValidationError(
-                {"quantidade": "Quantidade solicitada não disponível em estoque."}
+                {
+                    "quantidade": "Quantidade solicitada não disponível em estoque."
+                }
             )
         return data
 
@@ -40,8 +48,17 @@ class CriarEditarItenSerializer(ModelSerializer):
 class CriarEditarCompraSerializer(ModelSerializer):
     itens = CriarEditarItenSerializer(many=True)
     usuario = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    
+
     class Meta:
         model = Compra
-        fields = ("itens","usuario")
+        fields = ("itens", "usuario")
+    
+    def create(self, validated_data): 
+        itens = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item in itens:
+            item["preco_item"] = item["produto"].preco
+            ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
+        return compra
     
