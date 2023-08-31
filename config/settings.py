873cac56466cc4d1c0b8a1f8ceef50fc1c0353cb
eugreg <daiaegregory@gmail.com>
2023-08-31 14:25:27 -0300

    comentando o autenticaTIONS

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.DjangoModelPermissions",),
+    # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.DjangoModelPermissions",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 SPECTACULAR_SETTINGS = {
