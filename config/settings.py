65b2869c512405facdf0ae9472eadcf63b9e97e1
eugreg <daiaegregory@gmail.com>
2023-08-31 14:21:10 -0300

    instalacao do gunicorn

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = "django-insecure-=v$+v1nic!mskz#q#7hf$ke7#xy)vh)&8l8p$+oa@kekoy1sab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
