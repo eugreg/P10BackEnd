b877df3213bfe9684003f7252df028dbd5a4a372
eugreg <daiaegregory@gmail.com>
2023-08-31 15:59:02 -0300

    pesquisa por nome

--- P10/views/produto.py
+++ P10/views/produto.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from P10.models import Produtos
 from P10.serializers import( ProdutosListSerializer, ProdutosDetailSerializer, ProdutosSerializer)
@@ -8,6 +9,7 @@ class ProdutosViewSet(ModelViewSet):
     queryset = Produtos.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_fields = [ "categoria"]
+    search_fields = ["nome", "categoria", "fornecedor"]
 
     serializer_class = {
         "list": ProdutosListSerializer,
