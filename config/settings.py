435a7e921136fbdf0d5ff353100692deb944dd11
eugreg <daiaegregory@gmail.com>
2023-09-01 10:53:04 -0300

    arruma pfv

--- config/settings.py
+++ config/settings.py
@@ -1,25 +1,21 @@
 import os
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+load_dotenv()
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
     "django-insecure-=v$+v1nic!mskz#q#7hf$ke7#xy)vh)&8l8p$+oa@kekoy1sab"
 )
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 MODE = os.getenv("MODE")
 
