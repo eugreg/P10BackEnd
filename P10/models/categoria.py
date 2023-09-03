f8fa103e61cd30d179dbd5f05bcdb750f805deed
horodeski <geovana.horodeski06@gmail.com>
2023-09-03 13:39:18 -0300

    Adição de icones em categorias

--- P10/models/categoria.py
+++ P10/models/categoria.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
-
+    materialCommunityIcons = models.CharField(max_length=50)
+    
     def __str__(self):
         return self.descricao
